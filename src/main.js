73f9bb042894296766ca59d111e6817769e07cf3
zhāo <270281156@qq.com>
2019-02-18 21:14:21 +0800

    first comnmit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+// eslint 配置，允许 new 一个实例后不赋值，我们没有使用 eslint，如果有，则下一行注释不可缺少
+/* eslint-disable no-new */
+
+//创建一个新的vue实例
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
